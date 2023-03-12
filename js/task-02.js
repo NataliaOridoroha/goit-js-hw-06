7dfa6d2d7efcd913421a08bf705ea00d42955e48
NataliaOridoroha <nk121418@gmail.com>
2023-03-12 15:53:02 +0200

    goit-js-hw-06

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,15 @@ const ingredients = [
   'Condiments',
 ];
 
+const navItemEl = document.createElement('li');
+navLinkEl.classList.add('item');
+
+const navLinkEl = document.createElement('a');
+navLinkEl.textContent = 'Potatoes';
+
+navItemEl.appendChild(navLinkEl);
+console.log(navItemEl);
+
+const navEl = document.querySelector('ingredients');
+
+navEl.appendChild(navItemEl);
