257d9e518d9e7a655bff68cf02711d561c471877
NataliaOridoroha <nk121418@gmail.com>
2023-03-13 20:59:09 +0200

    goit-js-hw-06

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,27 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+
+const imagesEl = document.querySelector('.gallery');
+
+const elements = images.map(option => {
+
+const titleEl = document.createElement('li');
+titleEl.classList.add('image');
+titleEl.insertAdjacentHTML('beforeend',
+ '<img src=https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 alt="White and Black Long Fur Cat" >',
+ );
+titleEl.insertAdjacentHTML('beforeend', 
+'<img src=https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 alt="Orange and White Koi Fish Near Yellow Koi Fish" >');
+titleEl.insertAdjacentHTML('beforeend', 
+'<img src=https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 alt="Group of Horses Running" >');
+return titleEl;
+});
+console.log(elements);
+imagesEl.appendChild(...elements);
+
+
+
+
