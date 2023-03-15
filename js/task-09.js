ba6367a44e23f66888e0ae8753dd3b9c780ff7d7
NataliaOridoroha <nk121418@gmail.com>
2023-03-15 23:15:49 +0200

    goit-js-hw-06

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+const hex = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E","F",];
+
+
+
+button.addEventListener("click", () => {
+  let HexColor = getRandomHexColor();
+  document.body.style.backgroundColor = HexColor;
+  color.textContent = HexColor;
+});
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
